4ae8fb4dbed0652792ea8e6b9542391de32f79be
ryanhe312 <ryanhe312@gmail.com>
2024-03-28 17:24:49 +0800

    update infer

--- demo.py
+++ demo.py
@@ -36,52 +36,57 @@ def single_forward(model, imgs_in, space_scale, time_scale):
         imgs_temp = imgs_in.new_zeros(b, n, c, h_n, w_n)
         imgs_temp[:, :, :, 0:h, 0:w] = imgs_in
 
-        time_Tensors = [torch.tensor([i / time_scale])[None].to(device) for i in range(time_scale)]
+        time_Tensors = [torch.tensor([i / time_scale])[None].to(device) for i in range(time_scale+1)]
         model_output = model(imgs_temp, time_Tensors, space_scale, test=True)
         return model_output
 
 
-os.makedirs(opt.out_path_lr, exist_ok=True)
-os.makedirs(opt.out_path_bicubic, exist_ok=True)
+# os.makedirs(opt.out_path_lr, exist_ok=True)
+# os.makedirs(opt.out_path_bicubic, exist_ok=True)
 os.makedirs(opt.out_path_ours, exist_ok=True)
 
-path_list = [os.path.join(opt.data_path, name) for name in sorted(os.listdir(opt.data_path))]
-index = 0
-for ind in tqdm(range(len(path_list) - 1)):
+subdirs = os.listdir(opt.data_path)
 
-    imgpath1 = os.path.join(path_list[ind])
-    imgpath2 = os.path.join(path_list[ind + 1])
+for subdir in tqdm(sorted(subdirs)):
+    data_path = os.path.join(opt.data_path, subdir)
 
-    img1 = cv2.imread(imgpath1, cv2.IMREAD_UNCHANGED)
-    img2 = cv2.imread(imgpath2, cv2.IMREAD_UNCHANGED)
+    path_list = [os.path.join(data_path, name) for name in sorted(os.listdir(data_path))]
+    index = 0
+    for ind in tqdm(range(0, len(path_list)-2, 2)):
+        os.makedirs(os.path.join(opt.out_path_ours, subdir), exist_ok=True)
 
-    '''
-    We apply down-sampling on the original video
-    in order to avoid CUDA out of memory.
-    You may skip this step if your input video
-    is already of relatively low resolution.
-    '''
-    img1 = imresize_np(img1, 1 / 8, True).astype(np.float32) / 255.
-    img2 = imresize_np(img2, 1 / 8, True).astype(np.float32) / 255.
+        imgpath1 = os.path.join(path_list[ind])
+        imgpath2 = os.path.join(path_list[ind + 2])
 
-    Image.fromarray((np.clip(img1[:, :, [2, 1, 0]], 0, 1) * 255).astype(np.uint8)).save(
-        os.path.join(opt.out_path_lr, path_list[ind].split('/')[-1]))
+        img1 = cv2.imread(imgpath1, cv2.IMREAD_UNCHANGED)[:,:,:3]
+        img2 = cv2.imread(imgpath2, cv2.IMREAD_UNCHANGED)[:,:,:3]
 
-    imgs = np.stack([img1, img2], axis=0)[:, :, :, [2, 1, 0]]
-    imgs = torch.from_numpy(np.ascontiguousarray(np.transpose(imgs, (0, 3, 1, 2)))).float()[None].to(device)
+        '''
+        We apply down-sampling on the original video
+        in order to avoid CUDA out of memory.
+        You may skip this step if your input video
+        is already of relatively low resolution.
+        '''
+        img1 = img1.astype(np.float32) / 255.
+        img2 = img2.astype(np.float32) / 255.
 
-    output = single_forward(model, imgs, opt.space_scale, opt.time_scale)
+        # Image.fromarray((np.clip(img1[:, :, [2, 1, 0]], 0, 1) * 255).astype(np.uint8)).save(
+        #     os.path.join(opt.out_path_lr, path_list[ind].split('/')[-1].replace('.exr', '.png')))
 
-    '''
-    Save results of VideoINR and bicubic up-sampling.
-    '''
-    for out_ind in range(len(output)):
+        imgs = np.stack([img1, img2], axis=0)[:, :, :, [2, 1, 0]]
+        imgs = torch.from_numpy(np.ascontiguousarray(np.transpose(imgs, (0, 3, 1, 2)))).float()[None].to(device)
 
-        img = output[out_ind][0]
-        img = Image.fromarray((img.clamp(0., 1.).detach().cpu().permute(1, 2, 0) * 255).numpy().astype(np.uint8))
-        img.save(os.path.join(opt.out_path_ours, '{}.jpg'.format(index)))
+        # print(imgs.shape)
+        output = single_forward(model, imgs, opt.space_scale, opt.time_scale)
+        # print(len(output), output[0].shape)
 
-        HH, WW = img1.shape[0] * 4, img1.shape[1] * 4
-        img = Image.fromarray((np.clip(img1[:, :, [2, 1, 0]], 0, 1) * 255).astype(np.uint8)).resize((WW, HH),Image.BICUBIC)
-        img.save(os.path.join(opt.out_path_bicubic, '{}.jpg'.format(index)))
-        index += 1
+        '''
+        Save results of VideoINR and bicubic up-sampling.
+        '''
+        for out_ind in range(len(output)):
+            img = output[out_ind][0]
+            img = Image.fromarray((img.clamp(0., 1.).detach().cpu().permute(1, 2, 0) * 255).numpy().astype(np.uint8))
+            img.save(os.path.join(opt.out_path_ours, subdir, '{:08d}.png'.format(index+1)))
+            index += 1
+
+        index -= 1
